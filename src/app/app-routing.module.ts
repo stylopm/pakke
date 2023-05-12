588d5cdb4889a6c660f7d8ea7a120e8802412955
stylopm <apmurillo99@hotmail.com>
2023-05-11 21:54:26 -0600

    feat: Preparo todo para instalar boostrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-
 const routes: Routes = [
   {
     path: '',
+    loadChildren: () => import('@pages/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'login',
     loadChildren: () => import('@pages/login/login.module').then((m) => m.LoginModule),
   },
   {
@@ -23,7 +26,10 @@ const routes: Routes = [
   {
     path: '**',
     pathMatch: 'full',
-    redirectTo: '/administration',
+    loadChildren: () =>
+    import('@pages/page-not-found/page-not-found.module').then(
+      (m) => m.PageNotFoundModule
+    ),
   },
 ];
 
--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './register.component';
+import { RegisterComponent } from '@pages/register/register.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: '', component: RegisterComponent }];
