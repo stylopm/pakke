690f817c8ce37499f6b6a568bc2e557d046c51f2
stylopm <apmurillo99@hotmail.com>
2023-05-12 01:25:11 -0600

    feat: Se agrega la tabla donde se muestra la información

--- src/app/pages/panel/panel.component.ts
+++ src/app/pages/panel/panel.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PackagesService } from '@services/packages.service';
 
 @Component({
   selector: 'app-panel',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./panel.component.css']
 })
 export class PanelComponent {
+  data: any
+  constructor(
+    private ps: PackagesService,
+  ) {
+    this.loadPackages()  
+  }
 
+  loadPackages(){
+    this.ps.list().subscribe((res) => {
+      this.data= res['data']
+    })
+  }
 }
